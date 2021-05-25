77439e9a8cffb75460abef38759159045cb89b5d
CindyChow123 <CindyChow123>
2021-05-25 19:19:53 +0800

    save my flter

--- pages/home/home.js
+++ pages/home/home.js
@@ -74,7 +74,7 @@ Page({
                   wx.navigateTo({
                     url: '/pages/index/index',
                     success: function(res) {
-                      
+                      res.eventChannel.emit('getUrl',{path:img.path,width:img.width,height:img.height,type:Type}); 
                     },
                     fail: function(res){
                       console.log('Choose Picture Fail!')
